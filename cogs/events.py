8f9b0cf163598f2f75716ed36e1194d181025ab0
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2020-12-14 20:42:24 +0000

    Add files via upload

---
+++ cogs/events.py
@@ -0,0 +1,14 @@
+import discord
+
+from discord.ext import commands
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Online.")
+
+def setup(bot):
+    bot.add_cog(Events(bot))
